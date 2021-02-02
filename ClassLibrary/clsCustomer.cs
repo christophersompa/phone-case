2bcb816f6d725b29ea434faac089bfe10f7e1785
Christopher-George Sompa <P2445277@my365.dmu.ac.uk>
2021-02-02 10:23:42 +0000

    creating the properties for customer table

--- ClassLibrary/clsCustomer.cs
+++ ClassLibrary/clsCustomer.cs
@@ -1,6 +1,19 @@
-﻿namespace ClassLibrary
+﻿using System;
+
+namespace ClassLibrary
 {
     public class clsCustomer
     {
+        public bool Active { get; set; }
+        public DateTime DateAdded { get; set; }
+        public int CustomerNo { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string Address { get; set; }
+        public string PostCode { get; set; }
+        public string Town { get; set; }
+        public string Title { get; set; }
+        public string MobilePhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
     }
 }
