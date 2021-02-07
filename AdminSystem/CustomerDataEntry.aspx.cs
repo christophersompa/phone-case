cf96aeaf7327b45a2364fee7eab3f3e3a5cb171c
Christopher-George Sompa <P2445277@my365.dmu.ac.uk>
2021-02-07 12:30:09 +0000

    adding the customer presentation layer

--- AdminSystem/CustomerDataEntry.aspx.cs
+++ AdminSystem/CustomerDataEntry.aspx.cs
@@ -4,11 +4,24 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsCustomer
+        clsCustomer AnCustomer = new clsCustomer();
+        //capture the customer no
+        AnCustomer.CustomerNo = Int32.Parse(txtCustomerNo.Text);
+        //store the customer in the session object
+        Session["AnCustomer"] = AnCustomer;
+        //navigate to the viewer page
+        Response.Redirect("CustomerViewer.aspx");
     }
 }
