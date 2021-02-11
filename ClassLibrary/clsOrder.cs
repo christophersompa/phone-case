f55e2493ddfca7bb2ba90e9279c135105bcdb654
JoshuaAdediji28 <p2524824@my365.dmu.ac.uk>
2021-02-11 16:42:19 +0000

    Revert "Created propeties for order processing table design"
    This reverts commit d6cd25ec4e21b414403bc26556be9222afd0da53.

--- ClassLibrary/clsOrder.cs
+++ ClassLibrary/clsOrder.cs
@@ -1,21 +1,9 @@
-﻿using System;
-
-namespace ClassLibrary
+﻿namespace ClassLibrary
 {
     public class clsOrder
     {
         public clsOrder()
         {
         }
-
-        public bool Dispatched { get; set; }
-        public DateTime OrderDate { get; set; }
-        public int OrderNo { get; set; }
-        public int TrackingNo { get; set; }
-        public string CustomerName { get; set; }
-        public string CustomerEmail { get; set; }
-        public int ProductNo { get; set; }
-        public int Quantity { get; set; }
-        public int TotalPrice { get; set; }
     }
 }
