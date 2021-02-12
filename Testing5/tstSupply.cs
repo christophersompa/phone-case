117ac2869da3f33bb60a00aff456a218f166312b
Lance Stewart <P2445868@my365.dmu.ac.uk>
2021-02-12 23:26:47 +0000

    Added testing and class for Supply Management

---
+++ Testing5/tstSupply.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing5
+{
+    [TestClass]
+    public class tstSupply
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //create an instance of the class we want to create 
+            clsSupply AnSupply = new clsSupply();
+            //test to see that it exists 
+            Assert.IsNotNull(AnSupply);
+        }
+    }
+}
