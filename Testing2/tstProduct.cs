2dba1bb1716acfd7f27ec1af8c8071c086749fb5
Mohomed Vindhani <P2449263@my365.dmu.ac.uk>
2021-02-13 14:46:25 +0000

    Added testing and class management

---
+++ Testing2/tstProduct.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstProduct
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsProduct AnProduct = new clsProduct();
+            Assert.IsNotNull(AnProduct);
+        }
+    }
+}
