6a7bb57c2de6472a4bb1b19a672f14ae536de39e
Mohomed Vindhani <P2449263@my365.dmu.ac.uk>
2021-02-18 17:44:56 +0000

    Added a basic presentation Layer

--- AdminSystem/ProductViewer.aspx.cs
+++ AdminSystem/ProductViewer.aspx.cs
@@ -4,11 +4,17 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //create a new instance of clsProduct
+        clsProduct AnProduct = new clsProduct();
+        //get the data from the session object
+        AnProduct = (clsProduct)Session["AnProduct"];
+        //display the Product ID for this entry
+        Response.Write(AnProduct.ProductId);
     }
 }
