d1d2c3c91e22ee33cd47bf3f6adbf3e9adf46e1a
Mohomed Vindhani <P2449263@my365.dmu.ac.uk>
2021-03-08 22:17:18 +0000

    Presentation Layer of Find method

--- AdminSystem/ProductDataEntry.aspx.cs
+++ AdminSystem/ProductDataEntry.aspx.cs
@@ -5,7 +5,6 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using ClassLibrary;
-
 public partial class _1_DataEntry : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
@@ -23,5 +22,27 @@ public partial class _1_DataEntry : System.Web.UI.Page
         Session["AnProduct"] = AnProduct;
         //Navigate to the viewer page
         Response.Redirect("ProductViewer.aspx");
+    }
+
+    protected void btnFind_Click(object sender, EventArgs e)
+    {
+        //create an instance of the address class
+        clsProduct AnProduct = new clsProduct();
+        //variable to store the primary key
+        Int32 ProductId;
+        //variable to store the result of the find operation
+        Boolean Found=false;
+        //get the primary key entered by the user
+        ProductId = Convert.ToInt32(txtProductId.Text);
+        //find the record
+        Found = AnProduct.Find(ProductId);
+        //if found
+        if (Found == true)
+        {
+            //display the values of the properties in the form
+            txtProductName.Text = AnProduct.ProductName;
+            txtSmartphoneBrand.Text = AnProduct.SmartPhoneBrand;
+            txtSmartphoneModel.Text = AnProduct.SmartPhoneModel;
+        }
     }
 }
